31d454f3c372f4e7b540a28a02e753c07f7f7e08
flarnrules <breifenberg11@gmail.com>
2024-02-03 01:30:42 -0600

    first commit

---
+++ web/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const grid = document.getElementById('imageGrid');
+    for (let i = 1; i <= 1000; i++) {
+        let img = document.createElement('img');
+        img.src = `../nfts/images/${i}.png`; // Update the path as needed
+        grid.appendChild(img);
+    }
+});
